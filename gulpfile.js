20299ef4b77fee5c5f221bd0047594e96ea4b313
Stanislav Sergeev <stas.webdev@gmail.com>
2018-02-25 16:49:48 +0500

    initial

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const del = require('del');
+const runSequence = require('run-sequence');
+const path = require('path');
+
+const srcDir = path.resolve(__dirname, 'src');
+const distDir = path.resolve(__dirname, 'dist');
+
+gulp.task('default', (done) => {
+  runSequence('del', ['pug', 'sass', 'js'], done);
+});
+
+gulp.task('watch', () => gulp.watch(srcDir + '/**/*', ['default']));
+
+gulp.task('pug', () => {
+  return gulp.src(srcDir + '/*.pug')
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(distDir));
+});
+
+gulp.task('sass', () => {
+  return gulp.src(srcDir + '/*.sass')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest(distDir));
+});
+
+gulp.task('js', () => {
+  return gulp.src(srcDir + '/*.js')
+    .pipe(gulp.dest(distDir));
+});
+
+gulp.task('del', () => {
+  return del(distDir + '/**/*');
+});
